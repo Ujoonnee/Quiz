3173fd272b124b1625df1e4fb9f2eeff83a2e798
Ujoonnee <dnwns0933@naver.com>
2022-01-03 16:12:38 +0900

    220103

---
+++ Java100/src/conditional/Quiz2.java
@@ -0,0 +1,16 @@
+package conditional;
+
+public class Quiz2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		// if ~ else 조건문의 사용법을 예제로 설명해보시오.
+		// 아래 코드에서 틀린 곳을 찾아서 수정하시오.
+		// 이 문제는 자바의 if ~ else 조건문을 사용할 수 있는지에 대해서 묻는 문제이다.
+		
+		
+	}
+
+}
